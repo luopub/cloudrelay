4445cc0eaf7af0cee93d355da7d6a5bef1ae11f2
luopub <22909077@qq.com>
2026-02-28 02:49:41 +0800

    update

---
+++ pull-gitee.bat
@@ -0,0 +1,1 @@
+git push https://github.com/luopub/cloudrelay.git
---
+++ push-git.bat
@@ -0,0 +1,4 @@
+git add .
+git commit -m "update"
+git push https://gitee.com/luopub/cloudrelay.git
+git push https://github.com/luopub/cloudrelay.git
